5b11f82992e3fdbfc9336de4501fbae03824a90e
eclolus2023 <eclolus2023@129SE23-2D4MMS3.ad.campus-eni.fr>
2023-09-05 14:03:13 +0200

    Servlet/jsp connexion

--- src/main/java/fr/eni/trocenchere/servlets/ServletConnexion.java
+++ src/main/java/fr/eni/trocenchere/servlets/ServletConnexion.java
@@ -2,38 +2,33 @@ package fr.eni.trocenchere.servlets;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletConnexion
- */
+
+@WebServlet("/ServletConnexion")
 public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletConnexion() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// Dirige vers la jsp connexion
+		
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/JSP/connexion.jsp");
+		rd.forward(request, response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+		
 		doGet(request, response);
 	}
 
