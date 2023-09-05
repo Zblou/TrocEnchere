04c2d8fa82aa88bc243afd94647e999219b370fa
eclolus2023 <eclolus2023@129SE23-2D4MMS3.ad.campus-eni.fr>
2023-09-05 11:37:35 +0200

    Merge branch 'master' of https://github.com/Zblou/TrocEnchere.git

--- src/main/java/fr/eni/trocenchere/servlets/ServletConnexion.java
+++ src/main/java/fr/eni/trocenchere/servlets/ServletConnexion.java
@@ -2,26 +2,36 @@ package fr.eni.trocenchere.servlets;
 
 import java.io.IOException;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-public class ServletInscription extends HttpServlet {
+/**
+ * Servlet implementation class ServletConnexion
+ */
+public class ServletConnexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletConnexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Dirige vers la page d'inscription
-		
-		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/JSP/inscription.jsp");
-		rd.forward(request, response);
-		
+		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
