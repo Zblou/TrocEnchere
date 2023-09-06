4674760f8a8d7c5443998109ba4eee3a69180ea1
dthibaud2023 <dthibaud2023@129SE23-3D4MMS3.ad.campus-eni.fr>
2023-09-06 11:46:21 +0200

    Merge branch 'master' of https://github.com/Zblou/TrocEnchere.git

--- src/main/java/fr/eni/trocenchere/dal/DAO/DAOUtilisateur.java
+++ src/main/java/fr/eni/trocenchere/dal/DAO/DAOUtilisateur.java
@@ -1,5 +1,26 @@
 package fr.eni.trocenchere.dal.DAO;
 
-public interface DAOUtilisateur  {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+
+import fr.eni.trocenchere.bo.Utilisateur;
+import fr.eni.trocenchere.dal.DALException;
+import fr.eni.trocenchere.dal.ErrorCodeDAL;
+import jakarta.servlet.RequestDispatcher;
+
+public interface DAOUtilisateur {
+	
+    boolean checkForUniquePseudoAndMail(String pseudo, String mail) throws DALException;
+    boolean checkForUniquePseudo(String pseudo) throws DALException;
+    boolean checkForUniqueMail(String mail) throws DALException;
+    Utilisateur selectUtilisateurByPseudo(String pseudo) throws DALException;
+    HashMap<Integer, String> selectUtilisateursWithCurrentAuction() throws DALException;
+    void updateCredit(int noUtilisateur, int newCredit) throws DALException;
 
 }
+
+
