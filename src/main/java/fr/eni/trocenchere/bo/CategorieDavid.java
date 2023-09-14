483f172f5a7262c63e4f19b2aad1fee6226e857b
dthibaud2023 <dthibaud2023@129SE23-3D4MMS3.ad.campus-eni.fr>
2023-09-14 14:35:14 +0200

    ça marche

--- src/main/java/fr/eni/trocenchere/bo/CategorieDavid.java
+++ src/main/java/fr/eni/trocenchere/bo/CategorieDavid.java
@@ -3,39 +3,42 @@ package fr.eni.trocenchere.bo;
 import java.util.ArrayList;
 
 public class CategorieDavid {
-	private String nom;
+
+	private int idCategorie;
+	private String libelle;
 	private ArrayList<String> listCategorie = new ArrayList<>();
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+	
 	public ArrayList<String> getListCategorie() {
 		return listCategorie;
 	}
 	public void setListCategorie(ArrayList<String> listCategorie) {
 		this.listCategorie = listCategorie;
 	}
-	public CategorieDavid(String nom, ArrayList<String> listCategorie) {
-		super();
-		this.nom = nom;
-		this.listCategorie = listCategorie;
+	public int getIdCategorie() {
+		return idCategorie;
 	}
-	public CategorieDavid(String nom) {
-		super();
-		this.nom = nom;
-	}
-	public CategorieDavid(int int1, String string) {
-		// TODO Auto-generated constructor stub
-	}
-	public void setIdCategorie(int int1) {
-		// TODO Auto-generated method stub
-		
+	public void setIdCategorie(int idCategorie) {
+		this.idCategorie = idCategorie;
 	}
 	public String getLibelle() {
-		// TODO Auto-generated method stub
-		return null;
+		return libelle;
 	}
-
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	public CategorieDavid() {
+		super();
+	}
+	
+	public CategorieDavid(String libelle) {
+		super();
+		this.libelle = libelle;
+	}
+	
+	public CategorieDavid(int idCategorie, String libelle) {
+		super();
+		this.idCategorie = idCategorie;
+		this.libelle = libelle;
+	}
 }
--- src/main/webapp/WEB-INF/JSP/AjoutCategorie.jsp
+++ src/main/webapp/WEB-INF/JSP/AjoutCategorie.jsp
@@ -2,7 +2,7 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
+<head >
 <meta charset="UTF-8">
  <meta http-equiv="refresh" content="5;url=/votre_application/Categorie.jsp">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
